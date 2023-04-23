018473ca93eef4e216b227cd8b788985dc99210b
Glitch (glitch-hello-node) <none>
2023-04-23 22:06:30 +0000

    🍲😻 Checkpoint ./server.js:639386/4542 ./public/index.html:639386/568 ./package.json:639386/1798 ./public/index.js:639386/3814

--- server.js
+++ server.js
@@ -1,119 +1,38 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+// the express package will run our server
+const express = require("express");
+const app = express();
+app.use(express.static("public")); // this line tells the express app to 'serve' the public folder to clients
 
-const path = require("path");
+// HTTP will expose our server to the web
+const http = require("http").createServer(app);
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
+// start our server listening on port 8080 for now (this is standard for HTTP connections)
+const server = app.listen(8080);
+console.log("Server is running on http://localhost:8080");
+
+/////SOCKET.IO///////
+const io = require("socket.io")().listen(server);
+
+const peers = {};
+
+io.on("connection", (socket) => {
+  console.log(
+    "Someone joined our server using socket.io.  Their socket id is",
+    socket.id
+  );
+
+  peers[socket.id] = {};
+
+  console.log("Current peers:", peers);
+
+  socket.on("msg", (data) => {
+    console.log("Got message from client with id ", socket.id, ":", data);
+    let messageWithId = { from: socket.id, data: data };
+    socket.broadcast.emit("msg", messageWithId);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Someone with ID", socket.id, "left the server");
+    delete peers[socket.id];
+  });
 });
-
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
-
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
-
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
-}
-
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
-
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
-
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
-
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
