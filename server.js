b3128667695073c2b7c5b632805a467ad1d0af1b
Glitch (glitch-hello-node) <none>
2023-05-02 00:12:45 +0000

    😅🌟 Checkpoint ./server.js:44163808/2051

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ const http = require("http").createServer(app);
 const server = app.listen(8080);
 console.log("Server is running on http://localhost:8080");
 
-
-// start our websocket server
+/////SOCKET.IO///////
 const io = require("socket.io")().listen(server);
 
 const peers = {};
@@ -21,10 +20,17 @@ io.on("connection", (socket) => {
     "Someone joined our server using socket.io.  Their socket id is",
     socket.id
   );
-  
-  peers[socket.id] = {};
 
-  console.log("Current peers:", peers);
+  // Make sure to send the client all existing peers
+  socket.emit("introduction", peers);
+
+  // tell other clients that a new peer joined
+  io.emit("newPeerConnected", socket.id);
+
+  peers[socket.id] = {
+    position: [0, 0.5, 0],
+    rotation: [0, 0, 0, 1], // stored as XYZW values of Quaternion
+  };
 
   socket.on("msg", (data) => {
     console.log("Got message from client with id ", socket.id, ":", data);
@@ -32,8 +38,33 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("msg", messageWithId);
   });
 
+  // whenever the client moves, update their movements in the clients object
+  socket.on("move", (data) => {
+    if (peers[socket.id]) {
+      peers[socket.id].position = data[0];
+      peers[socket.id].rotation = data[1];
+    }
+  });
+
+  // Relay simple-peer signals back and forth
+  socket.on("signal", (to, from, data) => {
+    if (to in peers) {
+      io.to(to).emit("signal", to, from, data);
+    } else {
+      console.log("Peer not found!");
+    }
+  });
+
   socket.on("disconnect", () => {
     console.log("Someone with ID", socket.id, "left the server");
+
+    io.sockets.emit("peerDisconnected", socket.id);
+
     delete peers[socket.id];
   });
 });
+
+// update all clients with peer data every 100 milliseconds (around 10 times per second)
+setInterval(() => {
+  io.sockets.emit("peers", peers);
+}, 100);
